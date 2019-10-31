87497d0a1e8beb6df959ae44c677c42b320a656c
LezardValeth97 <themasterofwind1997@gmail.com>
2019-10-31 18:46:28 +0700

    Change README

---
+++ labwork/build/CMakeFiles/labwork.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/labwork.dir/src/labwork_generated_jpegloader.cu.o"
+  "CMakeFiles/labwork.dir/src/labwork_generated_labwork.cu.o"
+  "CMakeFiles/labwork.dir/src/timer.cpp.o"
+  "labwork.pdb"
+  "labwork"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/labwork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
